a6c9d8c02c038da07a19e7530a65746605e611ae
dieu-huyen <dieunhhe173250@fpt.edu.vn>
2026-09-15 08:17:51 +0700

    feat: complete Lab 2 exercise 1

---
+++ lib/lab2/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Khai báo biến bằng các kiểu dữ liệu khác nhau.
+  int age = 23;
+  double height = 1.50;
+  String name = 'Dieu';
+  bool isStudent = true;
+
+  // In các biến bằng cách sử dụng nội suy chuỗi.
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height m');
+  print('Is student: $isStudent');
+
+    // Nội suy chuỗi với một biểu thức.
+  print('Next year, age will be ${age + 1}.');
+}
